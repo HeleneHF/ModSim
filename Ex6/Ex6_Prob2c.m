4f0e7a1368ff2a581d12c6dc0709a0b2e55be768
HeleneHF <helenehf@stud.ntnu.no>
2017-02-20 18:33:16 +0100

    problem 2

--- Ex6/Ex6_Prob2c.m
+++ Ex6/Ex6_Prob2c.m
@@ -12,7 +12,7 @@ h_0 = 2;        % [m] Initial value
 
 
 f = @(t,h)(-C_v/A)*sqrt(rho*g*h);    % The function for dh/dt
-tspan = [0 1];                      % The time span we want to simulate
+tspan = [0 1];                       % The time span we want to simulate
 
 % Solving
 [t,h] = ode45(f,tspan,h_0);
@@ -20,3 +20,6 @@ tspan = [0 1];                      % The time span we want to simulate
 % % Plot
  plot(t,h,'o-');
 
+ %% Comments
+%  The steplengths decrases as h approaches 0 as we would expect judging
+%  from Problem 4b.
