7863c3e712a2e177b1f8f9eadabc3228e2309194
HeleneHF <helenehf@stud.ntnu.no>
2017-02-20 18:32:12 +0100

    Final version

--- Ex6/Ex6_Prob3b.m
+++ Ex6/Ex6_Prob3b.m
@@ -6,8 +6,23 @@
 syms 'lambda';
 syms 'h';
 
-A = (1/12)*[12-3*lambda*h, 3*lambda*h; -3*lambda*h, 12-5*lambda*h];
+% A = (1/12)*[12-3*lambda*h, 3*lambda*h; -3*lambda*h, 12-5*lambda*h];
+% I = eye(2);
+% 
+% star = inv(I-A);
+
+A = (h*lambda/12)*[3 -3; 3 5];
 I = eye(2);
+b_T = (1/4)*[1 3];
 
-star = inv(I-A);
+R = 1 + (lambda*h)*b_T*(inv(I-(lambda*h*A)))*ones(2,1);
 
+% Controlling Problem 4b
+syms 'h';
+syms 'rho';
+syms 'g';
+
+x = 1/(sqrt(rho*g*h));
+diff(x);
+
+    
